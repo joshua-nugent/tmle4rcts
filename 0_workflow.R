da367b6a5c306177fe4de8489eeb6c52f9e89c68
Joshua Nugent <joshuanugent@gmail.com>
2022-07-16 14:47:54 -0700

    adding actual code

--- 0_workflow.R
+++ 0_workflow.R
@@ -10,8 +10,42 @@ check() # then...
 # @description @examples @examplesIf
 # @export @family @inheritParams @param
 # @returns @seealso @rdname
+# To wrap blocks to be less then 80 characters wide: 
+#  use Ctrl/Cmd + Shift + /
+#  Or menu: code -> re-flow comment
+
+# To other documentation:
+#   \code{\link{function}}: function in this package.
+# \code{\link[MASS]{abbey}}: function in another package.
+# \link[=dest]{name}: link to dest, but show name.
+# \code{\link[MASS:abbey]{name}}: link to function in another package, but show name.
+# \linkS4class{abc}: link to an S4 class.
+# To the web:
+#   \url{http://rstudio.com}: a url.
+# \href{http://rstudio.com}{Rstudio}:, a url with custom link text.
+# \email{hadley@@rstudio.com} (note the doubled @): an email address.
+
+# formatting with LATEX-style...
+# \emph{will give italics}
+# \strong{will give bold}
+# \code{inline code format}
+# \preformatted{for multiline code}
+# \itemize{
+    # \item First thing
+    # \item Second thing
+# }
+# \tabular, etc.
+
+# Special characters:
+#   Use @@ to get @
+#   Use \% to get %
+#   Use \\ to get \
+
+# use @inheritParams AnotherFunctionWithASharedArgument
+# to copy parameters from that other function (not recursively tho)
+
 # ... then...
-document()
+devtools::document()
 use_package_doc()
 check()
 
