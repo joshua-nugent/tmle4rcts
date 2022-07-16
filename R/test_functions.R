6eda658999d6b29c4738b9018bea7ede5965e338
Joshua Nugent <joshuanugent@gmail.com>
2022-07-16 14:18:54 -0700

    maybe working?

--- R/test_functions.R
+++ R/test_functions.R
@@ -1,5 +1,16 @@
 
 
+#' Test Me Out
+#' @description Run the main function in this package. Best funtion ever. Butts.
+#'
+#' @param x The input object
+#'
+#' @return A character string of gross "humor."
+#' @export
+#'
+#' @examples
+#' test_me_out(1)
+#' test_me_out("Yes")
 test_me_out <- function(x){
   if(x == "Yes"){
     return("Butt")
